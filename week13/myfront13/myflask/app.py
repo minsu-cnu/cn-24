5a15cfdbd13f4e53e12f25725c6e6e329bf09135
배민수 <duecost@gmail.com>
2024-11-30 16:36:07 +0900

    myfront13 추가

---
+++ week13/myfront13/myflask/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, Blueprint, render_template  # Flask 및 관련 모듈
+import urllib.request  # HTTP 요청을 위해
+import json  # JSON 데이터를 다루기 위해
+
+
+endpoint = 'http://15.164.102.226:8080/pastebin/api'
+
+app = Flask(__name__)
+bp = Blueprint('mybp', __name__, 
+               static_folder='static',
+               static_url_path='/pastebin/static',
+               template_folder='templates',
+               url_prefix='/pastebin')
+
+@bp.route(f'/', methods=['GET'])
+@bp.route(f'/index.html', methods=['GET'])
+def get_index():
+    count_users = 0
+    url = f'{endpoint}/users/'
+    data = None
+    headers = {'Accept': 'application/json'}
+    method = 'GET'
+    req = urllib.request.Request(url=url,
+                                 data=data,
+                                 headers=headers,
+                                 method=method)
+    with urllib.request.urlopen(req) as f:
+        data = json.loads(f.read())
+        count_users = len(data)
+
+    count_pastes = 0
+    url = f'{endpoint}/pastes/'
+    data = None
+    headers = {'Accept': 'application/json'}
+    method = 'GET'
+    req = urllib.request.Request(url=url,
+                                 data=data,
+                                 headers=headers,
+                                 method=method)
+    with urllib.request.urlopen(req) as f:
+        data = json.loads(f.read())
+        count_pastes = len(data)
+
+    return render_template('index.html', 
+                           count_users=count_users,
+                           count_pastes=count_pastes)
+
+app.register_blueprint(bp)
---
+++ week13/myfront13/myflask/static/myscript.js
@@ -0,0 +1,23 @@
+async function fetchRequestWithError() {
+    try {
+      const url = `http://15.164.102.226:8080/pastebin/api/pastes/`;
+      const response = await fetch(url);
+      if (response.status >= 200 && response.status < 400) {
+        const data = await response.json();
+        pdiv = document.getElementById('pastes');
+        pdiv.clear();
+        for (var key in data) {
+          ndiv = document.createElement('div');
+          ndiv.innerHTML = `<h3> ${data[key]['title']} </h3><p> ${data[key]['content']}</p><hr>`;
+          pdiv.appendChild(ndiv);
+        }
+      } else {
+        console.log(`${response.statusText}: ${response.status} error`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  fetchint = setInterval(fetchRequestWithError, 10*1000);
+  
