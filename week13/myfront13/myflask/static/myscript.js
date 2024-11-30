2b0eb8abc39fb2c10825f80e2ca8241ffde3f805
배민수 <duecost@gmail.com>
2024-11-30 18:15:59 +0900

    feat: 메인 화면 paste 리스트 중복 해결

--- week13/myfront13/myflask/static/myscript.js
+++ week13/myfront13/myflask/static/myscript.js
@@ -4,10 +4,13 @@ async function fetchRequestWithError() {
       const response = await fetch(url);
       if (response.status >= 200 && response.status < 400) {
         const data = await response.json();
+        pdiv = document.getElementById('pastes');
+        while (pdiv.firstChild) {
+          pdiv.removeChild(pdiv.firstChild);
+        }
         for (var key in data) {
           ndiv = document.createElement('div');
           ndiv.innerHTML = `<h3> ${data[key]['title']} </h3><p> ${data[key]['content']}</p><hr>`;
-          pdiv = document.getElementById('pastes');
           pdiv.appendChild(ndiv);
         }
       } else {
